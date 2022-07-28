a2119b375bebd5430b7a9596ecd4e4856ea7bb00
Uku3lig <thelololomail@gmail.com>
2022-07-28 20:50:53 +0200

    initial commit

---
+++ src/main/java/net/uku3lig/popcounter/PopCounter.java
@@ -0,0 +1,11 @@
+package net.uku3lig.popcounter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PopCounter implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
