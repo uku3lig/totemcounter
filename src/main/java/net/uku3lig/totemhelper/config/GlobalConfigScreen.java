79b84d242b418f889b2008960dc4f87db30444d1
Uku3lig <thelololomail@gmail.com>
2022-07-30 00:13:02 +0200

    add reset counter button

--- src/main/java/net/uku3lig/totemhelper/config/GlobalConfigScreen.java
+++ src/main/java/net/uku3lig/totemhelper/config/GlobalConfigScreen.java
@@ -5,6 +5,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonListWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.ScreenTexts;
@@ -16,6 +17,7 @@ import java.io.IOException;
 @Log4j2
 public class GlobalConfigScreen extends GameOptionsScreen {
     private final GlobalConfig config;
+    private ButtonListWidget buttons;
 
     public GlobalConfigScreen(Screen parent, GlobalConfig config) {
         super(parent, MinecraftClient.getInstance().options, Text.translatable("totemhelper.config"));
@@ -26,16 +28,21 @@ public class GlobalConfigScreen extends GameOptionsScreen {
     @Override
     protected void init() {
         super.init();
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 155, this.height / 2 - 10, 150, 20,
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 75, this.height / 2 - 35, 150, 20,
                 Text.translatable("totemhelper.config.pop"), button -> client.setScreen(new PopCounterConfigScreen(this, config))));
-        this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height / 2 - 10, 150, 20,
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 75, this.height / 2 - 10, 150, 20,
                 Text.translatable("totemhelper.config.display"), button -> client.setScreen(new TotemDisplayConfigScreen(this, config))));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 75, this.height / 2 + 15, 150, 20, Text.translatable("totemhelper.reset"), button -> TotemHelper.getPops().clear()));
         this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, this.height - 27, 200, 20, ScreenTexts.DONE, button -> this.client.setScreen(this.parent)));
+
+        buttons = new ButtonListWidget(this.client, this.width, this.height, 32, this.height - 32, 25);
+        this.addSelectableChild(buttons);
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
+        buttons.render(matrices, mouseX, mouseY, delta);
         DrawableHelper.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
     }
