f2878f64a54b00665e8f6bdbdc8570f2dd0a6d9a
Uku3lig <thelololomail@gmail.com>
2022-07-29 23:41:16 +0200

    fix small config screen issue

--- src/main/java/net/uku3lig/totemhelper/config/GlobalConfigScreen.java
+++ src/main/java/net/uku3lig/totemhelper/config/GlobalConfigScreen.java
@@ -2,9 +2,12 @@ package net.uku3lig.totemhelper.config;
 
 import lombok.extern.log4j.Log4j2;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 import net.uku3lig.totemhelper.TotemHelper;
 
@@ -27,6 +30,14 @@ public class GlobalConfigScreen extends GameOptionsScreen {
                 Text.translatable("totemhelper.config.pop"), button -> client.setScreen(new PopCounterConfigScreen(this, config))));
         this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height / 2 - 10, 150, 20,
                 Text.translatable("totemhelper.config.display"), button -> client.setScreen(new TotemDisplayConfigScreen(this, config))));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, this.height - 27, 200, 20, ScreenTexts.DONE, button -> this.client.setScreen(this.parent)));
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        DrawableHelper.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
+        super.render(matrices, mouseX, mouseY, delta);
     }
 
     @Override
