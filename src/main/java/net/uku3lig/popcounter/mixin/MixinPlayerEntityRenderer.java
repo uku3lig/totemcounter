f360183aa3d36ebfaccfd34253e28c9e6be02de6
Uku3lig <thelololomail@gmail.com>
2022-07-29 10:30:24 +0200

    reset counter on death

--- src/main/java/net/uku3lig/popcounter/mixin/MixinPlayerEntityRenderer.java
+++ src/main/java/net/uku3lig/popcounter/mixin/MixinPlayerEntityRenderer.java
@@ -23,6 +23,7 @@ public abstract class MixinPlayerEntityRenderer extends LivingEntityRenderer<Abs
     @Redirect(method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
     at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"))
     public void renderPopCounter(LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> instance, Entity entity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+        if (!entity.isAlive()) PopCounter.getPops().remove(entity.getUuid());
         if (PopCounter.getPops().containsKey(entity.getUuid())) {
             text = text.copy().append(" | -").append(String.valueOf(PopCounter.getPops().get(entity.getUuid())));
         }
