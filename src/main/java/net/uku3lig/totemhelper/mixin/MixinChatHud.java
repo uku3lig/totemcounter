903429d7bf6f5f883535ae9c426fc1b8a57184ba
Uku3lig <thelololomail@gmail.com>
2022-07-31 11:48:08 +0200

    add auto-reset for leg and mcpvp

---
+++ src/main/java/net/uku3lig/totemhelper/mixin/MixinChatHud.java
@@ -0,0 +1,22 @@
+package net.uku3lig.totemhelper.mixin;
+
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.text.Text;
+import net.uku3lig.totemhelper.TotemHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Mixin(ChatHud.class)
+public class MixinChatHud {
+    private static final List<String> roundEndMessages = Arrays.asList("Winners:", "has won the round.", "has won the game!");
+
+    @Inject(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", at = @At("HEAD"))
+    public void checkForDeath(Text message, int messageId, int timestamp, boolean refresh, CallbackInfo ci) {
+        if (roundEndMessages.stream().anyMatch(m -> message.getString().contains(m))) TotemHelper.getPops().clear();
+    }
+}
