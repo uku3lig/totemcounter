11279cca0907dec6d093e51d160223b6b62fad7a
uku <uku3lig@gmail.com>
2022-11-28 17:11:16 +0100

    use Position#getOption

--- src/main/java/net/uku3lig/totemcounter/config/TotemDisplayConfigScreen.java
+++ src/main/java/net/uku3lig/totemcounter/config/TotemDisplayConfigScreen.java
@@ -1,14 +1,11 @@
 package net.uku3lig.totemcounter.config;
 
-import com.mojang.serialization.Codec;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.text.Text;
 import net.uku3lig.ukulib.config.ConfigManager;
 import net.uku3lig.ukulib.config.Position;
 import net.uku3lig.ukulib.config.screen.SubConfigScreen;
-
-import java.util.Arrays;
 
 public class TotemDisplayConfigScreen extends SubConfigScreen<TotemCounterConfig.TotemDisplayConfig, TotemCounterConfig> {
 
@@ -20,9 +17,7 @@ public class TotemDisplayConfigScreen extends SubConfigScreen<TotemCounterConfig
     protected SimpleOption<?>[] getOptions(TotemCounterConfig.TotemDisplayConfig config) {
         return new SimpleOption[] {
                 SimpleOption.ofBoolean("totemcounter.config.enabled", config.isEnabled(), config::setEnabled),
-                new SimpleOption<>("totemcounter.config.display.position", SimpleOption.emptyTooltip(), SimpleOption.enumValueText(),
-                        new SimpleOption.PotentialValuesBasedCallbacks<>(Arrays.asList(Position.values()), Codec.STRING.xmap(Position::valueOf, Position::name)),
-                        config.getPosition(), config::setPosition),
+                Position.getOption(config::getPosition, config::setPosition),
                 SimpleOption.ofBoolean("totemcounter.config.display.defaultTotem", config.isUseDefaultTotem(), config::setUseDefaultTotem),
                 SimpleOption.ofBoolean("totemcounter.config.colors", config.isColors(), config::setColors),
                 SimpleOption.ofBoolean("totemcounter.config.display.coloredXpBar", config.isColoredXpBar(), config::setColoredXpBar),
