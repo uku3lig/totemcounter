54593bed3b37c38ebc30f54165a1f35689b5236d
uku <uku3lig@gmail.com>
2022-11-28 17:09:50 +0100

    port to ukulib O.3

--- src/main/java/net/uku3lig/totemcounter/config/GlobalConfigScreen.java
+++ src/main/java/net/uku3lig/totemcounter/config/GlobalConfigScreen.java
@@ -2,8 +2,8 @@ package net.uku3lig.totemcounter.config;
 
 import lombok.extern.log4j.Log4j2;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.option.SimpleOption;
-import net.minecraft.text.Text;
+import net.minecraft.client.option.Option;
+import net.minecraft.text.TranslatableText;
 import net.uku3lig.totemcounter.TotemCounter;
 import net.uku3lig.ukulib.config.ConfigManager;
 import net.uku3lig.ukulib.config.screen.AbstractConfigScreen;
@@ -12,12 +12,12 @@ import net.uku3lig.ukulib.utils.Ukutils;
 @Log4j2
 public class GlobalConfigScreen extends AbstractConfigScreen<TotemCounterConfig> {
     public GlobalConfigScreen(Screen parent, ConfigManager<TotemCounterConfig> manager) {
-        super(parent, Text.translatable("totemcounter.config"), manager);
+        super(parent, new TranslatableText("totemcounter.config"), manager);
     }
 
     @Override
-    protected SimpleOption<?>[] getOptions(TotemCounterConfig config) {
-        return new SimpleOption[] {
+    protected Option[] getOptions(TotemCounterConfig config) {
+        return new Option[] {
                 Ukutils.createOpenButton("totemcounter.config.pop", "", parent -> new PopCounterConfigScreen(parent, manager)),
                 Ukutils.createOpenButton("totemcounter.config.display", "", parent -> new TotemDisplayConfigScreen(parent, manager)),
                 Ukutils.createButton("totemcounter.reset", "", parent -> TotemCounter.getPops().clear())
