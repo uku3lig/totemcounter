c3e56306e2cae621a8d8fb09f73dc717c7bacb06
uku <uku3lig@gmail.com>
2023-02-15 23:41:47 +0100

    backport 1.2.0 to 1.16.5

--- src/main/java/net/uku3lig/totemcounter/config/PopCounterConfigScreen.java
+++ src/main/java/net/uku3lig/totemcounter/config/PopCounterConfigScreen.java
@@ -1,6 +1,8 @@
 package net.uku3lig.totemcounter.config;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.CyclingOption;
 import net.minecraft.client.option.Option;
 import net.minecraft.text.TranslatableText;
 import net.uku3lig.totemcounter.TotemCounter;
@@ -14,12 +16,14 @@ public class PopCounterConfigScreen extends SubConfigScreen<TotemCounterConfig.P
 
     @Override
     protected Option[] getOptions(TotemCounterConfig.PopCounterConfig config) {
+        CyclingOption tabOption = TotemCounter.onOffOption("totemcounter.config.tab", config::isShowInTab, config::setShowInTab);
+        tabOption.setTooltip(MinecraftClient.getInstance().textRenderer.wrapLines(new TranslatableText("totemcounter.experimental"), 200));
+
         return new Option[] {
                 TotemCounter.onOffOption("totemcounter.config.enabled", config::isEnabled, config::setEnabled),
                 TotemCounter.onOffOption("totemcounter.config.pop.separator", config::isSeparator, config::setSeparator),
-                TotemCounter.onOffOption("totemcounter.config.colors", config::isColors, config::setColors)
-                CyclingOption.create("totemcounter.config.tab", new TranslatableText("totemcounter.experimental"),
-                        opt -> config.isShowInTab(), (opt, option, value) -> config.setShowInTab(value))
+                TotemCounter.onOffOption("totemcounter.config.colors", config::isColors, config::setColors),
+                tabOption
         };
     }
 }
