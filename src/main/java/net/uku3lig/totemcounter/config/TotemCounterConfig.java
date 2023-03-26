31eab593221b54a10c4eff1af42e288fcf184179
uku <uku3lig@gmail.com>
2023-03-26 17:40:34 +0200

    improve performance in MixinEntityRenderer
    1.19.3 backport

--- src/main/java/net/uku3lig/totemcounter/config/TotemCounterConfig.java
+++ src/main/java/net/uku3lig/totemcounter/config/TotemCounterConfig.java
@@ -11,10 +11,11 @@ import net.uku3lig.ukulib.config.ISubConfig;
 public class TotemCounterConfig implements IConfig<TotemCounterConfig> {
     private PopCounterConfig counterConfig;
     private TotemDisplayConfig displayConfig;
+    private ExperimentalConfig experimentalConfig;
 
     @Override
     public TotemCounterConfig defaultConfig() {
-        return new TotemCounterConfig(new PopCounterConfig().defaultConfig(), new TotemDisplayConfig().defaultConfig());
+        return new TotemCounterConfig(new PopCounterConfig().defaultConfig(), new TotemDisplayConfig().defaultConfig(), new ExperimentalConfig().defaultConfig());
     }
 
     @Getter
@@ -50,6 +51,20 @@ public class TotemCounterConfig implements IConfig<TotemCounterConfig> {
         @Override
         public PopCounterConfig defaultConfig() {
             return new PopCounterConfig(true, true, true, false);
+        }
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ExperimentalConfig implements ISubConfig<ExperimentalConfig, TotemCounterConfig> {
+        private boolean disableArmorStands;
+        private double maxDistance;
+
+        @Override
+        public ExperimentalConfig defaultConfig() {
+            return new ExperimentalConfig(false, 20);
         }
     }
 }
