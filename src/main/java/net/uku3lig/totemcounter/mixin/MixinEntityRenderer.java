d76f5ff9b8428a0a58011f713a7a6cc9c1088e63
uku <hi@uku.moe>
2024-08-12 10:36:08 +0200

    feat(mixin): use mixinextras stuff

--- src/main/java/net/uku3lig/totemcounter/mixin/MixinEntityRenderer.java
+++ src/main/java/net/uku3lig/totemcounter/mixin/MixinEntityRenderer.java
@@ -1,7 +1,11 @@
 package net.uku3lig.totemcounter.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import lombok.extern.slf4j.Slf4j;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -10,44 +14,39 @@ import net.uku3lig.totemcounter.TotemCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(EntityRenderer.class)
 @Slf4j
 public class MixinEntityRenderer {
-    @ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/EntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V"))
-    public void label(Args args) {
-        Entity entity = args.get(0);
-        Text text = args.get(1);
+    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/EntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V"))
+    public void label(EntityRenderer<Entity> instance, Entity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float tickDelta, Operation<Void> original) {
+        if (entity.getWorld().isClient && text != null) {
+            // try to get a player's uuid, sometimes by scanning the name of an armor stand
+            PlayerEntity player = switch (entity) {
+                case PlayerEntity p -> p;
+                case ArmorStandEntity ignored -> {
+                    final String stringText = text.getString();
+                    if (stringText.isBlank()) yield null;
 
-        if (!entity.getWorld().isClient) return;
-        if (text == null) return;
+                    // TODO: maybe implement some sort of client-side cache for faster lookups? currently this being computed every frame lol
+                    for (PlayerEntity p : entity.getWorld().getPlayers()) {
+                        int index = stringText.indexOf(p.getNameForScoreboard());
+                        if (isSurrounded(stringText, index, p.getNameForScoreboard().length())) continue;
 
-        final String stringText = text.getString();
-        if (stringText.isBlank()) return;
-
-        // try to get a player's uuid, sometimes by scanning the name of an armor stand
-        PlayerEntity player = switch (entity) {
-            case PlayerEntity p -> p;
-            case ArmorStandEntity ignored -> {
-                // TODO: maybe implement some sort of client-side cache for faster lookups? currently this being computed every frame lol
-                for (PlayerEntity p : entity.getWorld().getPlayers()) {
-                    int index = stringText.indexOf(p.getNameForScoreboard());
-                    if (isSurrounded(stringText, index, p.getNameForScoreboard().length())) continue;
-
-                    yield p;
+                        yield p;
+                    }
+                    yield null; // no matching player found
                 }
-                yield null; // no matching player found
+                default -> null;
+            };
+
+            if (player != null) {
+                if (!player.isAlive()) TotemCounter.getPops().remove(entity.getUuid());
+                text = TotemCounter.showPopsInText(player, text);
             }
-            default -> null;
-        };
+        }
 
-        if (player == null) return;
-        if (!player.isAlive()) TotemCounter.getPops().remove(entity.getUuid());
-
-        Text fixedText = TotemCounter.showPopsInText(player, text);
-        args.set(1, fixedText);
+        original.call(instance, entity, text, matrices, vertexConsumers, light, tickDelta);
     }
 
     // 2024 edit: i have no fucking clue what this does but sure uku3lig from the past, slay queen
