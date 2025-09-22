558d935111201568c4f5d12523a7458600a5cb39
uku <hi@uku.moe>
2025-09-22 19:02:50 +0200

    feat: migrate to kotlin buildscript

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("fabric-loom") version "1.11-SNAPSHOT"
+    id("io.freefair.lombok") version "9.0.0-rc2"
+}
+
+version = "${project.property("mod_version")}+mc${project.property("minecraft_version")}"
+group = project.property("maven_group") as String
+
+repositories {
+    maven {
+        url = uri("https://maven.uku3lig.net/releases")
+    }
+}
+
+dependencies {
+    // To change the versions see the gradle.properties file
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+
+    modImplementation(fabricApi.module("fabric-key-binding-api-v1", project.property("fabric_version") as String))
+    modImplementation(fabricApi.module("fabric-command-api-v2", project.property("fabric_version") as String))
+
+    modApi("net.uku3lig:ukulib:${project.property("ukulib_version")}")
+}
+
+base {
+    archivesName = project.property("archives_base_name") as String
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filteringCharset = "UTF-8"
+
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.base.archivesName.get()}" }
+    }
+}
